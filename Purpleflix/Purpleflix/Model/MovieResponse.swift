47600684ab09b44fad4aed0ca41db772966c3b09
Pedro Esli <pedroesli.vn@gmail.com>
2022-05-19 15:06:43 -0300

    Update: MovieRequest attributes

--- Purpleflix/Purpleflix/Model/MovieResponse.swift
+++ Purpleflix/Purpleflix/Model/MovieResponse.swift
@@ -7,13 +7,25 @@
 
 import Foundation
 
-struct MovieResponse: Decodable{
+struct MovieResponse: Decodable {
     var backdrop_path: String?
-    var budget: Int?
-    var genres: [GenresResponse]?
+    var budget: Int
+    var genres: [GenresResponse]
+    var id: Int
+    var imdb_id: String?
+    var original_language: String
+    var original_title: String
+    var overview: String? //Sinopse
+    var popularity: Int
+    var poster_path: String?
+    var revenue: Int
+    var runtime: Int //Duração do filme em minutos
+    var tagline: String? //Resumo
+    var title: String
+    var vote_average: Double
 }
 
 struct GenresResponse: Decodable {
-    var id: Int?
-    var name: String?
+    var id: Int
+    var name: String
 }
