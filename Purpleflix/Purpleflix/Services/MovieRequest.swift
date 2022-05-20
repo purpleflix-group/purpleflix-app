3db33a7fb92bb02debc0790c3e83a3d884b4b2c5
Pedro Esli <pedroesli.vn@gmail.com>
2022-05-20 17:03:09 -0300

    Update: GeneralRequest.swift

--- Purpleflix/Purpleflix/Services/MovieRequest.swift
+++ Purpleflix/Purpleflix/Services/MovieRequest.swift
@@ -8,20 +8,20 @@
 import Foundation
 import Alamofire
 
-class MovieRequest: GeneralRequest<MovieResponse> {
+class MovieRequest {
     
     static func getDetails(movieID: Int, result: @escaping (_ movie: MovieResponse?) -> Void) {
         let url = "https://api.themoviedb.org/3/movie/\(movieID)"
-        super.getDetails(url: url, result: result)
+        GeneralRequest<MovieResponse>.getDetails(url: url, result: result)
     }
     
     static func getRecommendations(movieID: Int, result: @escaping (_ movies: [MovieResponse]?) -> Void) {
         let url = "https://api.themoviedb.org/3/movie/\(movieID)/recommendations"
-        super.getRecommendations(url: url, result: result)
+        GeneralRequest<MovieResponse>.getRecommendations(url: url, result: result)
     }
     
     static func getWatchProvider(movieID: Int, result: @escaping (_ providers: [String:WatchProviderResponse]?) -> Void) {
         let url = "https://api.themoviedb.org/3/movie/\(movieID)/watch/providers"
-        super.getWatchProvider(url: url, result: result)
+        GeneralRequest<MovieResponse>.getWatchProvider(url: url, result: result)
     }
 }
