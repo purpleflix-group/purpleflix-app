016e98e1df6ce42b7da67b08558b323c59eddb10
Pedro Esli <pedroesli.vn@gmail.com>
2022-05-20 17:09:35 -0300

    Revert "Merge remote-tracking branch 'origin/feature-TVRequest' into development"
    This reverts commit a15ad6fe699b3cc98155f21d3bd43a609fcbe7fe, reversing
    changes made to 3db33a7fb92bb02debc0790c3e83a3d884b4b2c5.

--- Purpleflix/Purpleflix/Services/TVRequest.swift
+++ Purpleflix/Purpleflix/Services/TVRequest.swift
@@ -5,24 +5,8 @@
 //  Created by Pedro Ésli Vieira do Nascimento on 18/05/22.
 //
 
-import Alamofire
+import Foundation
 
-class TVRequest: GeneralRequest {
+class TVRequest {
     
-
-    static func getDetails(TVid: Int, result: @escaping (_ tv: TVReponse?) -> Void){
-        let url = "https://api.themoviedb.org/3/tv/\(TVid)"
-        super.getDetails(url: url, result: result)
-    }
-    
-    static func getRecommendations(tvId: Int, result: @escaping (_ tv: TVRecommendationsResponse?) -> Void) {
-        let url = "https://api.themoviedb.org/3/tv/\(tvId)/recommendations"
-        let parameters = [
-            "api_key": ApiKey.themoviedbkey,
-            "language": "pt-BR"
-        ]
-        AF.request(url, parameters: parameters).responseDecodable(of: TVRecommendationsResponse.self) { response in
-            result(response.value)
-        }
-    }
 }
