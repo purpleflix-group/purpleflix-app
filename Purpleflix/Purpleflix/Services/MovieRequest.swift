a3aa5265fa79fcf3d3b01ed8b173e1edabde9485
Pedro Esli <pedroesli.vn@gmail.com>
2022-05-20 15:58:48 -0300

    Refactor: Remade GeneralRequest.swift

--- Purpleflix/Purpleflix/Services/MovieRequest.swift
+++ Purpleflix/Purpleflix/Services/MovieRequest.swift
@@ -8,42 +8,20 @@
 import Foundation
 import Alamofire
 
-class MovieRequest: GeneralRequest {
+class MovieRequest: GeneralRequest<MovieResponse> {
     
     static func getDetails(movieID: Int, result: @escaping (_ movie: MovieResponse?) -> Void) {
         let url = "https://api.themoviedb.org/3/movie/\(movieID)"
-        super.getDetails(url: url, type: MovieResponse.self, result: result)
+        super.getDetails(url: url, result: result)
     }
     
     static func getRecommendations(movieID: Int, result: @escaping (_ movies: [MovieResponse]?) -> Void) {
         let url = "https://api.themoviedb.org/3/movie/\(movieID)/recommendations"
-        let parameters = [
-            "api_key": ApiKey.themoviedbkey,
-            "language": "pt-BR"
-        ]
-        AF.request(url, parameters: parameters).response { response in
-            guard let data = response.data else {
-                result(nil)
-                return
-            }
-            do {
-                let json = try JSON(data: data)
-                var movies: [MovieResponse] = []
-                for value in json["results"].arrayValue {
-                    let movie = try JSONDecoder().decode(MovieResponse.self, from: value.rawData())
-                    movies.append(movie)
-                }
-                result(movies)
-            }
-            catch {
-                print("[Error][MovieRequest][getRecommendations]: \(error)")
-                result(nil)
-            }
-        }
+        super.getRecommendations(url: url, result: result)
     }
     
     static func getWatchProvider(movieID: Int, result: @escaping (_ providers: [String:WatchProviderResponse]?) -> Void) {
         let url = "https://api.themoviedb.org/3/movie/\(movieID)/watch/providers"
-        getWatchProvider(url: url, result: result)
+        super.getWatchProvider(url: url, result: result)
     }
 }
